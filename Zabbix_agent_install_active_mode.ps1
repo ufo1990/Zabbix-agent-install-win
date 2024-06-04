9c5241bc6dd6ac415c3741ff58f5c7b3b1aaca39
ufo1990 <dybiec1990@gmail.com>
2024-06-04 14:00:03 +0200

    Add files via upload

---
+++ Zabbix_agent_install_active_mode.ps1
@@ -0,0 +1,31 @@
+﻿$path = "C:\Users\$env:username\Downloads"
+
+#Download Zabbix agent
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest "https://cdn.zabbix.com/zabbix/binaries/stable/6.4/6.4.11/zabbix_agent2-6.4.11-windows-amd64-openssl.msi" -Outfile $path\zabbix_agent2-6.4.11-windows-amd64-openssl.msi
+
+if(Test-Path $path\zabbix_agent2-6.4.11-windows-amd64-openssl.msi){ 
+    
+    Write-host "File was downloaded."
+
+    Start-Sleep -Seconds 1
+
+    try {   
+        #Instalation and configuration Zabbix agent
+        msiexec /I $path\zabbix_agent2-6.4.11-windows-amd64-openssl.msi server=127.0.0.1 serveractive=127.0.0.1:10051 HOSTNAME=$env:computername ENABLEPATH=1 /qn
+    
+        Write-Host "Zabbix agent was installed and configured." 
+
+    } catch {
+        Write-Host "Error installed: $_"
+    }    
+ 
+    Start-Sleep -Seconds 3
+
+    #Removing download file Zabbix agent after installed
+    rm $path\zabbix_agent2-6.4.11-windows-amd64-openssl.msi
+
+    if(-not(Test-Path $path\zabbix_agent2-6.4.11-windows-amd64-openssl.msi)){
+       Write-host "File was removed."
+    }
+}
